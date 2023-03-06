efa18e225e9899a0b729d4f6aeb2ccd27659251b
albertjcuac <albertjcuac@gmail.com>
2023-03-06 09:51:34 +0100

    html and css responsive template

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+
+  <header class="header">
+    <input class="search" placeholder="Search for a character" @input="search"/>
+  </header>
+  <div class="container">
+    <aside class="facets">
+      <h2>Filters</h2>
+      <span>Status</span>
+      <ul class="filters">
+        <li class="filter"><label>Alive<input name="filter" type="checkbox"></label></li>
+        <li class="filter"><label>Dead<input name="dead" type="checkbox"></label></li>
+        <li class="filter"><label>Unknown<input name="unknown" type="checkbox"></label></li>
+      </ul>
+    </aside>
+    <main>
+      <section class="characters">
+        <article class="character">character 1
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 2
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 3
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 4
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 5
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 6
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 7
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 8
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 9
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+        <article class="character">character 10
+          <div class="description">example
+            <div class="status alive">alive</div>
+          </div>
+        </article>
+      </section>
+    </main>
+  </div>
+</template>
+<script lang="js">
+export default {
+  data() {
+    return {
+      characters: []
+    };
+  },
+  methods: {
+    search(event) {
+      const query = event.target.value;
+      fetch('https://rickandmortyapi.com/api/character/?name=' + query).then(response => response.json())
+          .then(data => {
+            this.characters = data.results;
+            console.log(this.characters);
+          });
+    }
+  }
+};
+</script>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+}
+
+.search {
+  width: 600px;
+}
+
+.characters {
+  display: flex;
+  flex-flow: row wrap;
+  list-style-type: none;
+  gap: 12px;
+
+}
+
+.character {
+  display: flex;
+  flex-flow: column;
+  width: 250px;
+  height: 350px;
+  border: 1px solid black;
+}
+
+.description {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.status {
+  padding: 5px;
+  color: white;
+}
+
+.alive {
+  background: green;
+}
+
+.dead {
+  background: red;
+}
+
+.unknown {
+  background: gray;
+}
+
+.filters {
+  width: 120px;
+  list-style-type: none;
+}
+
+.filter:hover {
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.filter {
+  margin-bottom: 8px;
+}
+
+.container {
+  display: flex;
+}
+
+@media screen and (max-width: 767px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+
+}
+</style>
