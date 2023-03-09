25b6d9af22b9ea5670f233534bde724c441b8848
albertjcuac <albertjcuac@gmail.com>
2023-03-09 12:26:29 +0100

    " added more information to hover card"

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -4,18 +4,30 @@
     <div class="description"> {{ character.name }}
       <div v-bind:class="['status',character.status.toLowerCase()]">{{ character.status }}</div>
     </div>
+    <div class="info" :class="{ 'show': showInfo }">
+      <p>Gender:{{ character.gender }}</p>
+      <p>Specie:{{ character.species }}</p>
+      <p>Origin:{{ character.origin.name }}</p>
+
+    </div>
   </article>
 </template>
 
 <script>
 export default {
   name: "CharacterCard",
-  props: ["character"]
+  props: ["character"],
+  data() {
+    return {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
 .character {
+  position: relative;
   display: flex;
   flex-flow: column;
   width: 15rem;
@@ -23,18 +35,31 @@ export default {
   border: 1px solid black;
   background-color: whitesmoke;
   border-radius: 50px;
+  transition: transform 0.5s ease;
+  transform-style: preserve-3d;
+}
+
+
+
+
+.character:hover {
+  transform: rotateY(180deg);
 }
 
 .description {
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  justify-content: space-between;
-
+  height: 100%;
+  padding: 1rem;
 }
+
 .character__image {
   border-top-left-radius: 50px;
   border-top-right-radius: 50px;
 }
+
 .status {
   padding: 5px;
   color: white;
@@ -50,5 +75,34 @@ export default {
 
 .unknown {
   background: gray;
+}
+
+.info {
+  border-radius: 50px;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  box-sizing: border-box;
+  transition: transform 0.5s ease;
+  transform-style: preserve-3d;
+  background-color: whitesmoke;
+}
+
+.info p {
+  margin: 0.5rem 0;
+}
+
+.show {
+  transform: rotateY(0deg);
 }
 </style>
